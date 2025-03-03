e841b33f73e09096827b11c3666440047c2a3ec0
misunderstood <2djohar@gmail.com>
2025-03-03 13:22:56 +0200

    initial commit

--- gui/setup.py
+++ gui/setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="gui",
     version="0.1",
-    py_modules=["ctk_windows"],  # Replace "gui" with the actual script name (e.g., gui.py)
+    packages=["gui"],  # Treats the gui folder as a package
 )
