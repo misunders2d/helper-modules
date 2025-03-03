ffbbf65f84fa761d3b6fc1c9f3bb317e18cfad8d
misunderstood <2djohar@gmail.com>
2025-03-03 13:28:46 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="gui",
     version="0.1",
-    packages=["gui"],  # Treats the gui folder as a package
+    packages=["gui"],  # Only includes the gui package
+    package_dir={"gui": "gui"},
 )
