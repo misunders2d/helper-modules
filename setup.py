38c7f9d51ffd3dceaef83064f3b0ea467c129341
misunderstood <2djohar@gmail.com>
2025-03-04 14:52:00 +0200

    trying whole package

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ctk_gui",
-    version="0.1",
-    packages=find_packages(),
-    package_dir={'': '.'}  # Important if not at root
+    name="helper_modules",           # Name of your package
+    version="0.1.0",                    # Version number
+    description="A collection of helper modules",
+    author="Serge",
+    author_email="2djohar@gmail.com",
+    packages=find_packages(),           # Automatically find all packages
+    install_requires=[                  # Dependencies (if any)
+        "customtkinter",
+        "tkcalendar"
+    ],
+    python_requires=">=3.6",            # Specify compatible Python versions
 )
