5513ad7f2de9ebc56a1d8f88d59cf236a73a63ee
misunderstood <2djohar@gmail.com>
2025-03-14 17:07:33 +0200

    adding decorators module

---
+++ utils/decorators.py
@@ -0,0 +1,10 @@
+from ctk_gui.ctk_windows import PopupError
+
+def error_checker(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            error_str = f'function {func.__name__} returned an error:\n{e}'
+            PopupError(message=error_str)
+    return wrapper
