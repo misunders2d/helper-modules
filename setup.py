1b2e0a9138d4efd2af2e808f57063d8fc5e3d45d
misunderstood <2djohar@gmail.com>
2025-07-24 13:17:06 +0300

    adding requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name="helper_modules",           # Name of your package
-    version="0.1.29",                    # Version number
+    version="0.1.30",                    # Version number
     description="A collection of helper modules",
     author="Sergey",
     author_email="2djohar@gmail.com",
     packages=find_packages(),           # Automatically find all packages
     install_requires=[                  # Dependencies (if any)
         "customtkinter",
+        "gdown",
+        "google-api-python-client",
+        "google-cloud-bigquery",
+        "gspread",
+        "numpy",
+        "openpyxl",
+        "python-dotenv",
+        "pandas",
+        "pandas-gbq",
         "tkcalendar",
-        "python-dotenv"
+        "xlsxwriter"
     ],
     python_requires=">=3.6",            # Specify compatible Python versions
 )
